e86c48d5c3f5451b74c8f7106508f8d9b53df6cd
mahendra <mahendra.yadav@brsoftech.in>
2020-01-28 17:35:24 +0530

    France Test Done

--- GitExample/ViewController.swift
+++ GitExample/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print(reverse(text: "stressed"))
+        print("done")
         // Do any additional setup after loading the view.
     }
 
